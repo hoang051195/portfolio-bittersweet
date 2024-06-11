8d353e183b67f9006a22df68eddad855dc7b498f
Lê Minh Hoàng <huongcaphemacchiato@gmail.com>
2024-06-11 23:05:51 +0700

    implement nav, scroll progress

---
+++ components/Nav.jsx
@@ -0,0 +1,42 @@
+// link (next js)
+import Link from "next/link";
+// framer motion
+import { motion } from "framer-motion";
+
+// next hooks
+import { usePathname } from "next/navigation";
+const links = [
+  { path: "/", name: "home" },
+  { path: "/projects", name: "my projects" },
+  { path: "/contact", name: "contact" },
+];
+
+const Nav = ({ containerStyles, linkStyles, underlineStyles }) => {
+  const path = usePathname();
+
+  return (
+    <nav className={`${containerStyles}`}>
+      {links.map((link, index) => {
+        return (
+          <Link
+            key={index}
+            href={link.path}
+            className={`capitalize ${linkStyles}`}
+          >
+            {link.path === path && (
+              <motion.span
+                initial={{ y: "-100%" }}
+                animate={{ y: 0 }}
+                transition={{ type: "tween" }}
+                layoutId="underline"
+                className={`${underlineStyles}`}
+              />
+            )}
+            {link.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
+};
+export default Nav;
